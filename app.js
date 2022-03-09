e3bd1125c3bea83a70ffb5eeed8df635201ebf71
tomrimington <94228643+tomrimington@users.noreply.github.com>
2022-03-09 15:02:10 +0000

    variable port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get("/", (req, res) => {
   res.send("welcome to the homepage bitchhhh");
 });
 
-app.listen(3000, () => {
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => {
   console.log("wasssuppp");
 });
