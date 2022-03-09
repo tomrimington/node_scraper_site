bb875c75624030021829a0d2e8834d938f01f902
tomrimington <94228643+tomrimington@users.noreply.github.com>
2022-03-09 15:35:08 +0000

    variable port

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+import * as pg from "pg";
+const { Pool } = pg.default;
+const pool = new Pool({
+  connectionlimit: 50,
+  host: "testdb.chzn6cnaazyl.eu-west-2.rds.amazonaws.com",
+  user: "postgres",
+  password: "Toothemoon69",
+  database: "Test_db",
+});
 
 app.get("/", (req, res) => {
   res.send("welcome to the homepage bitchhhh");
@@ -8,4 +17,14 @@ app.get("/", (req, res) => {
 const PORT = process.env.port || 3000;
 app.listen(PORT, () => {
   console.log("wasssuppp");
+});
+
+pool.query("select * from public.solana2 where id=1", (err, res) => {
+  if (!err) {
+    queryresult = result.rows[0][data];
+    return queryresult;
+  } else {
+    console.log(err.message);
+  }
+  console.log("row added");
 });
