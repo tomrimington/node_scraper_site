a534910a3a771d26e9734da4c74728e260562faa
tomrimington <94228643+tomrimington@users.noreply.github.com>
2022-03-09 16:19:25 +0000

    changed import for require

--- app.js
+++ app.js
@@ -8,26 +8,22 @@ const pool = new Pool({
   password: "Toothemoon69",
   database: "Test_db",
 });
-
-app.get("/", (req, res) => {
-  res.send("welcome to the homepage bitchhhh");
-});
+async () => {
+  app.get("/", (req, res) => {
+    pool.query("select * from public.solana2 where id=1", (err, res) => {
+      if (!err) {
+        queryresult = res.rows[0];
+        console.log(queryresult);
+      } else {
+        console.log(err.message);
+      }
+      console.log("row added");
+    });
+    res.send("welcome to the homepage bitchhhh", await queryresult);
+  });
+};
 
 const PORT = process.env.port || 3000;
 app.listen(PORT, () => {
   console.log("wasssuppp");
-});
-
-pool.query("select * from public.solana2 where id=1", (err, res) => {
-  if (!err) {
-    queryresult = res.rows[0];
-    console.log(queryresult);
-    app.get("/", (req, res) => {
-      res.send(queryresult);
-    });
-    return queryresult;
-  } else {
-    console.log(err.message);
-  }
-  console.log("row added");
 });
