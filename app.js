7b664c9dabdff8519f0580f2fe793a4a0a43b7bb
EC2 Default User <ec2-user@ip-172-31-40-137.eu-west-2.compute.internal>
2022-03-10 14:34:29 +0000

    hairy hairy prawns

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   
-    res.send("welcome to the homepage bitchhhh", queryresult);
+    res.send("welcome to the homepage bitchhhh");
   
 });
 
