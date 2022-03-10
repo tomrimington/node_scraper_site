1cb8eda35c9ef5b0f38d5c10127a5f3139e33fef
EC2 Default User <ec2-user@ip-172-31-40-137.eu-west-2.compute.internal>
2022-03-10 12:05:51 +0000

    nack to basics

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const app = express();
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionlimit: 50,
-  host: "testdb.chzn6cnaazyl.eu-west-2.rds.amazonaws.com",
-  user: "postgres",
-  password: "Toothemoon69",
-  database: "Test_db",
-});
+
 
 app.get("/", (req, res) => {
-  async () => {
-    pool.query("select * from public.solana2 where id=1", (err, res) => {
-      if (!err) {
-        let queryresult = res.rows[0];
-        console.log(queryresult);
-        return queryresult;
-      } else {
-        console.log(err.message);
-      }
-      console.log("row added");
-    });
+  
     res.send("welcome to the homepage bitchhhh", queryresult);
-  };
+  
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 8080;
 app.listen(PORT, () => {
   console.log("wasssuppp");
 });
+
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//   connectionlimit: 50,
+//   host: "testdb.chzn6cnaazyl.eu-west-2.rds.amazonaws.com",
+//   user: "postgres",
+//   password: "Toothemoon69",
+//   database: "Test_db",
+// });
+
+// async () => {
+//   pool.query("select * from public.solana2 where id=1", (err, res) => {
+//     if (!err) {
+//       let queryresult = res.rows[0];
+//       console.log(queryresult);
+//       return queryresult;
+//     } else {
+//       console.log(err.message);
+//     }
+//     console.log("row added");
+//   });
