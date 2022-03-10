e0996618519a9c174cd4bf5b417c12958f43e237
EC2 Default User <ec2-user@ip-172-31-40-137.eu-west-2.compute.internal>
2022-03-10 14:32:22 +0000

    added host

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const pool = new Pool({
   host: process.env.RDS_HOSTNAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+  port     : process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME
 });
 
 
