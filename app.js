71aac76928cf812d19b2159458a169013b897b71
EC2 Default User <ec2-user@ip-172-31-40-137.eu-west-2.compute.internal>
2022-03-10 15:19:41 +0000

    fuck

--- app.js
+++ app.js
@@ -14,21 +14,21 @@ app.listen(PORT, () => {
 });
 
 const { Pool } = require("pg");
-// const client = new Pool({
-//   host: "testdb.chzn6cnaazyl.eu-west-2.rds.amazonaws.com",
-//   user: "postgres",
-//   post: 5432,
-//   password: "Toothemoon69",
-//   database: "Test_db"
-// })
 const client = new Pool({
+  host: "testdb.chzn6cnaazyl.eu-west-2.rds.amazonaws.com",
+  user: "postgres",
+  post: 5432,
+  password: "Toothemoon69",
+  database: "Test_db"
+})
+// const client = new Pool({
   
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME
-});
+//   host: process.env.RDS_HOSTNAME,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   port     : process.env.RDS_PORT,
+//   database: process.env.RDS_DB_NAME
+// });
 
 
   client.query("select * from solana where id='1'", (err, res) => {
